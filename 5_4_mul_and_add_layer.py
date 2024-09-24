02bb4bc9efb74fb09d08c73aeb2db78b812b91cf
Shu Ogawara <shu.ogawara@linkers.net>
2024-09-24 16:17:59 +0900

    5.4

---
+++ 5_4_mul_and_add_layer.py
@@ -0,0 +1,78 @@
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+
+        return out
+    
+    def backward(self, dout):
+        dx = dout * self.y # x * y をxで偏微分
+        dy = dout * self.x # x * y をyで偏微分
+
+        return dx, dy
+
+apple = 100
+apple_num = 2
+tax = 1.1
+
+# layer
+mul_apple_layer = MulLayer()
+mul_tax_layer = MulLayer()
+
+# forward
+apple_price = mul_apple_layer.forward(apple, apple_num)
+price = mul_tax_layer.forward(apple_price, tax)
+
+print(price)
+
+# backward
+dprice = 1 # 最初の逆伝播は必ず1
+dapple_price, dtax = mul_tax_layer.backward(dprice)
+
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+print(dapple, dapple_num, dtax)
+
+
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+    
+    def backward(self, dout):
+        dx = dout * 1 # x + y をxで偏微分
+        dy = dout * 1 # x + y をyで偏微分
+        return dx, dy
+
+orange = 150
+orange_num = 3
+
+# layer
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+mul_tax_layer = MulLayer()
+
+# forward
+apple_price = mul_apple_layer.forward(apple, apple_num)
+orange_price = mul_orange_layer.forward(orange, orange_num)
+all_price = add_apple_orange_layer.forward(apple_price, orange_price)
+price = mul_tax_layer.forward(all_price, tax)
+
+print(price)
+
+# backward
+dprice = 1
+dall_price, dtax = mul_tax_layer.backward(dprice)
+dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)
+dorange, dorange_num = mul_orange_layer.backward(dorange_price)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+print(dapple_num, dapple, dorange, dorange_num, dtax)
